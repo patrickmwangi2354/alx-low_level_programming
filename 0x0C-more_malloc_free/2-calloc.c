f7e2110dd93ad47c80372b11055c09a0f582c45f
patrickmwangi2354 <patrickmwangi2354@gmail.com>
2023-03-16 15:06:40 -0700

    function that allocates memory for an array using malloc, and initializes it to zero using memset

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,23 +7,21 @@
  * *_memset - fills memory with a constant byte
  * @s: memory area to be filled
  * @b: char to copy
- * @n: number of times to copy b
+ * @n: number of times to copy
  * Return: pointer to the memory area s
  */
 
 char *_memset(char *s, char b, unsigned int n)
-
+void *ptr;
+if (nmemb == 0 || size == 0)
 {
-
-unsigned int i;
-
-for (i = 0; i < n; i++)
-
-{
-
-s[i] = b;
-
+return (NULL);
 }
-return (s);
-
+ptr = malloc(nmemb * size);
+if (ptr == NULL)
+{
+return (NULL);
+}
+memset(ptr, 0, nmemb * size);
+return (ptr);
 }
