ede9986859040bb3bfcb6a380d8bcce430e1fa16
patrickmwangi2354 <patrickmwangi2354@gmail.com>
2023-03-20 13:25:50 -0700

    print the file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
 
 /**
- *Main-print the Name of the file
- *The file name
- *Author Name
+ *Main - print the Name of the file
+ *Description: 'the programes description'
  *Return 0;
  */
 
-int ain(void)
+int main(void)
 {
 printf("%s\n", _FILE_);
 return (0);
